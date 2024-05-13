32d02435d0836a5ed910bcfa772537f2146be66e
Ash Mudaly <explosivefruits158@gmail.com>
2024-05-13 13:12:58 +1000

    sd

--- src/ADXL335.h
+++ src/ADXL335.h
@@ -49,7 +49,7 @@
 #define ADC_AMPLITUDE 1024
 #endif
 #ifndef ADC_REF
-#define ADC_REF 5  // ADC reference is 5v
+#define ADC_REF 5 // ADC reference is 5v
 #endif
 
 // accleration of X-AXIS is 0g, the voltage of X-AXIS is 1.22v
@@ -57,10 +57,10 @@
 #define ZERO_X 1.22
 #endif
 #ifndef ZERO_Y
-#define ZERO_Y 1.22  //
+#define ZERO_Y 1.22 //
 #endif
 #ifndef ZERO_Z
-#define ZERO_Z 1.25  //
+#define ZERO_Z 1.25 //
 #endif
 
 // sensitivity of X/Y/Z axis is 0.25v/g
@@ -68,14 +68,16 @@
 #define SENSITIVITY 0.25
 #endif
 
-class ADXL335 {
-  private:
+class ADXL335
+{
+private:
     void pinsInit();
     float scale;
-  public:
+
+public:
     void begin();
-    void getXYZ(int* x, int* y, int* z);
-    void getAcceleration(float* ax, float* ay, float* az);
+    void getXYZ(int *x, int *y, int *z);
+    void getAcceleration(float *ax, float *ay, float *az);
     float getAccelerationX();
     float getAccelerationY();
     float getAccelerationZ();
