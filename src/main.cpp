11241a851730a43a404444138676443aa1bb155e
Benjamout <Ben.R.Pedler@gmail.com>
2024-05-13 15:09:48 +1000

    Cleaned up

--- src/main.cpp
+++ src/main.cpp
@@ -16,21 +16,12 @@ const int testReadZ = A2;
 Pedometer pedometer;
 double time = 1;
 
-//LiquidCrystal_I2C lcd(0x27, 18, 2);
-
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   pedometer = Pedometer(0, 1, 2); // Initialize with sensor pins
-  // lcd.begin(16, 2); //Start and initialize the lcd display
-  // lcd.clear(); //Clear any text from the screen
-  // lcd.backlight();
-  // lcd.setCursor(0,1);
-  // lcd.print("Starting...");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   xValue = analogRead(testReadX);
   yValue = analogRead(testReadY);
   zValue = analogRead(testReadZ);
@@ -43,21 +34,9 @@ void loop() {
 //   Serial.println(zValue);
 //   delay(2);
 
-  // lcd.clear(); //Clear any text from the screen
-  // lcd.setCursor(0,0);
-  pedometer.getAxisData(xValue, yValue, zValue);
-  // Serial.print(xValue);
-  // Serial.print(yValue);
-  // Serial.print(zValue);
-  pedometer.stepAlgorithm(xValue, yValue, zValue);
 
-  // lcd.setCursor(0,1);
-  // lcd.print(xValue);
-  // lcd.setCursor(4,1);
-  // lcd.print(yValue);
-  // lcd.setCursor(8,1);
-  // lcd.print(zValue);
-  // delay(100);
+  pedometer.getAxisData(xValue, yValue, zValue); //reads and returns the x, y and z values
+  pedometer.stepAlgorithm(xValue, yValue, zValue); //determines if a step has been taken based on axis data
 
 delay(10);
 
