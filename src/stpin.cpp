dedac56674dffff1c8fd8eec794e5a55c7d8cad8
Arya N <64720029+LIlARYA@users.noreply.github.com>
2024-05-15 12:08:41 +1000

    self test now prints the values

--- src/stpin.cpp
+++ src/stpin.cpp
@@ -1,10 +1,11 @@
 #include "stpin.h"
 
 Stpin::Stpin(){
-    
-       
     // State control of the button
     buttonCounter = 0;
+    int xPin_ = 0;
+    int yPin_ = 1;
+    int zPin_ = 2;
 }
 
 void Stpin::stControl(LiquidCrystal_I2C lcd){
@@ -17,13 +18,34 @@ void Stpin::stControl(LiquidCrystal_I2C lcd){
         digitalWrite(2, HIGH); //st off
     }
     if (buttonCounter == 1) {
+
+        double x,y,z;
+        getAxisData(x,y,z);
+
         digitalWrite(2, LOW); // If the button is pressed provide 0V to gate, causing ST pin to activate
         // Print the state to the arduino
         lcd.clear();
         lcd.setCursor(0, 0);
         lcd.print("ADXL355 SelfTest");
+
+        // X printing
         lcd.setCursor(0, 1);
-        lcd.print("Routine Active!");
+        lcd.print("X: ");
+        lcd.setCursor(3, 1);
+        lcd.print(x);
+
+        // Y printing
+        lcd.setCursor(7, 1);
+        lcd.print("Y: ");
+        lcd.setCursor(9,1);
+        lcd.print(y);
+
+        // Z printing
+        lcd.setCursor(11, 1);
+        lcd.print("Z: ");
+        lcd.setCursor(13, 1);
+        lcd.print(z);
+
     }
         // If the button is pressed for a second time exit ST routine
     if(buttonCounter == 2){
@@ -32,7 +54,8 @@ void Stpin::stControl(LiquidCrystal_I2C lcd){
 }
 
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 0b40d2eaf00c40870257d75f9b061679daf9c4d3
+void Stpin::getAxisData(double &x, double &y, double &z) {
+  x = analogRead(xPin_); // Read sensor data
+  y = analogRead(yPin_);  
+  z = analogRead(zPin_);
+}
--- src/stpin.h
+++ src/stpin.h
@@ -6,8 +6,11 @@ class Stpin {
 public:
     Stpin(); // Default constructor
     void stControl(LiquidCrystal_I2C lcd);
+    void getAxisData(double &x, double &y, double &z);
 
   private:
     int buttonCounter;
-    
+    int xPin_;
+    int yPin_;
+    int zPin_;
 };
